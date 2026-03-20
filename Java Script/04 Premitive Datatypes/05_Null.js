bff98d130c51f69bc5d8d33248d780b3a23936a8
Vinay Saini <2officialvinay@gmail.com>
2026-03-20 22:11:38 +0530

    Null datatypes in js

---
+++ Java Script/04 Premitive Datatypes/05_Null.js
@@ -0,0 +1,7 @@
+// In JavaScript, null is a primitive value that represents the intentional
+// absence of any object value. It is used to signify that a variable, object,
+// or property does not have a value.
+
+let range = null;
+console.log(typeof(range));
+console.log(range);
