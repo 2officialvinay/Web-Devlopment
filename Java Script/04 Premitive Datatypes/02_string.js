921100e3e9ed24ac83a892e1a795112f099eb3f0
Vinay Saini <2officialvinay@gmail.com>
2026-03-20 21:35:26 +0530

    Strings in Js

---
+++ Java Script/04 Premitive Datatypes/02_string.js
@@ -0,0 +1,13 @@
+let str1 = "Mobile";
+console.log(typeof(str1));
+
+let str2 = "Charger";
+console.log(typeof(str2));
+
+// Concatenation of strings using '+' :
+let str3 = str1 + " and " + str2 + " are made for each other.";
+console.log(str3);
+
+// Concatenation using string interpolation using template literals(``):
+let str4 = `${str1} and ${str2} are made for each other.`;
+console.log(str4);
