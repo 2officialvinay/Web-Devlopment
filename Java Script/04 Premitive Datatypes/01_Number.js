cb06c205bb4824626e420ee0c89d01cd33b87056
Vinay Saini <2officialvinay@gmail.com>
2026-03-20 21:22:30 +0530

    Understanding Numbers in premitive DT in Js

---
+++ Java Script/04 Premitive Datatypes/01_Number.js
@@ -0,0 +1,14 @@
+let integerNumber = 34;
+console.log(typeof(integerNumber));
+
+let floatNumber = 23.4;
+console.log(typeof(floatNumber));
+
+let sum = integerNumber + floatNumber;
+console.log(sum);
+console.log(typeof(sum));
+
+let strInt = "23";
+console.log(typeof(strInt));
+let parsedInteger = parseInt(strInt);
+console.log(typeof(parsedInteger));
