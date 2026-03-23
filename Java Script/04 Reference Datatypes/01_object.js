cb3b979bb70c4a6c63381271e254144e62fc3e9e
Vinay Saini <2officialvinay@gmail.com>
2026-03-23 18:25:17 +0530

    objects in js

---
+++ Java Script/04 Reference Datatypes/01_object.js
@@ -0,0 +1,12 @@
+let obj = {
+    name:"Vinay",
+    age:21,
+    weight:68,
+    height:"5 Feets 7 inch",
+    Hello: function(){
+        console.log("Hello Everyone!");
+    }
+};
+
+console.log(obj);
+obj.Hello();
