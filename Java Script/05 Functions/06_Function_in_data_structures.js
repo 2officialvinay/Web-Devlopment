8583afc1e7ca3b5c6a525d353fc816748467ad3d
Vinay Saini <2officialvinay@gmail.com>
2026-03-31 01:26:50 +0530

    Function in array

---
+++ Java Script/05 Functions/06_Function_in_data_structures.js
@@ -0,0 +1,18 @@
+const arr = [
+    function(a,b){
+        return a+b;
+    },
+    function(a,b){
+        return a-b;
+    },
+    function(a,b){
+        return a*b;
+    },
+    function(a,b){
+        return a/b;
+    }
+];
+
+let ans = arr[3];
+let result = ans(10,5);
+console.log(result);
